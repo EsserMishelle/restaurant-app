a6aa5ce4cb8147227beba3e3862df3087460e8a2
Esser, Mishelle <EsserMishelle@gmail.com>
2024-03-08 15:23:20 -0500

    _redirects

--- frontend/src/utilities/items-api.js
+++ frontend/src/utilities/items-api.js
@@ -1,7 +1,6 @@
 import sendRequest from "./send-request";
 
 const BASE_URL = "/api/items";
-// const BASE_URL = "https://restaurant-app-nnv7.onrender.com/api/items";
 
 export function getAll() {
   return sendRequest(BASE_URL);
