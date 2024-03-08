5279d095b33c5ca9805ab34123737c2cf0aba30c
Esser, Mishelle <EsserMishelle@gmail.com>
2024-03-08 09:15:21 -0500

    login and signup routes

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,12 +1,19 @@
 // routes/api/users.js
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const usersCtrl = require('../../controllers/api/users');
+const usersCtrl = require("../../controllers/api/users");
 
 // POST /api/users
-router.post('/', usersCtrl.create);
+router.post("/", usersCtrl.create);
 //POST /api/users/login
-router.post('/login', usersCtrl.logIn);
+router.post("/login", usersCtrl.logIn);
 
+router.get("/", usersCtrl.create);
+router.get("/login", (req, res) => {
+  res.render("/login");
+});
+router.get("/signup", (req, res) => {
+  res.render("/signup");
+});
 module.exports = router;
