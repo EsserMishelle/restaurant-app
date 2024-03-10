f04dcb16f4ce54525a6c55229de9371250363bfa
Esser, Mishelle <EsserMishelle@gmail.com>
2024-03-09 21:15:03 -0500

    adding model to MenuListItem--new user

--- frontend/src/components/MenuListItem/MenuListItem.js
+++ frontend/src/components/MenuListItem/MenuListItem.js
@@ -1,13 +1,30 @@
 import styles from "./MenuListItem.module.css";
 // import { Link } from "react-router-dom";
+import MenuItemModal from "../Modal/MenuItemModal";
+import MenuItemDetail from "../../pages/Menu/MenuItemDetail";
+import { useState } from "react";
+
 export default function MenuListItem({ menuItems }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const handleItemClick = (item) => {
+    setSelectedItem(item);
+    setIsModalOpen(true);
+  };
+
   const items = menuItems.map((item, _id) => (
-    <div className={styles.MenuListItem} key={_id}>
+    <div
+      className={styles.MenuListItem}
+      key={_id}
+      onClick={() => handleItemClick(item)}
+      style={{ cursor: "pointer" }}
+    >
       {item.img && (
         <img
           src={item.img}
           alt={item.name}
-          style={{ maxWidth: "150px", height: "auto" }}
+          style={{ maxWidth: "250px", height: "auto", border: "1px solid red" }}
         />
       )}
       <h1>{item.name}</h1>
@@ -15,32 +32,12 @@ export default function MenuListItem({ menuItems }) {
       <h1>${item.price}</h1>
     </div>
   ));
-  return <main className={styles.MenuListItem}>{items}</main>;
+  return (
+    <main className={styles.MenuListItem}>
+      {items}
+      <MenuItemModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+        {selectedItem && <MenuItemDetail item={selectedItem} />}
+      </MenuItemModal>
+    </main>
+  );
 }
-
-//   <div className={styles.MenuListItem}>
-//     <Link to={`/menu/category/${menuItems._id}`} className="menu-item-link">
-//     <div className={styles.img + " " + "flex-ctr-ctr"}>
-//       {menuItems.img && <img
-//         src={menuItems.img}
-//         alt={menuItems.name}
-//         style={{ maxWidth: "150px", height: "auto" }}
-//       />}
-//     </div>
-//     <div className={styles.name}>{menuItems.name}</div>
-//     </Link>
-//     <div className={styles.buy}>
-//       <span>${menuItems.price.toFixed(2)}</span>
-
-//       <button
-//         className="btn-sm"
-//         onClick={() => handleAddToOrder(menuItems._id)}
-//       >
-//         ADD
-//       </button>
-//     </div>
-//   </div>
-// );
-
-// console.log(items);
-//   return <main className={styles.MenuListItem}>{items}</main>;}
